388f080b821ea379e399ad2e7e420952fef8dd1d
Jascha Kanngiesser <jascha.kanngiesser@sap.com>
2021-11-08 11:05:23 +0100

    update ing-postbox-download-all

--- ing-postbox-download-all.js
+++ ing-postbox-download-all.js
@@ -15,6 +15,19 @@
 // @homepageURL https://github.com/ja-ka/violentmonkey
 // ==/UserScript==
 
+const NAME = "Alle herunterladen";
+
+const download = async (url, name) => new Promise((res, rej) => {
+  GM_download({ url, name, onprogress: (progress) => {
+    if (progress.status === 200) {
+      res();
+    }
+  }, onerror: rej, onabort: rej, ontimeout: rej });
+});
+
+let abort = false;
+let loading = false;
+
 (function () {
     $(document).ready(function () {
       $('.content-header__button-wrapper--lg').after(VM.createElement("button", {
@@ -26,20 +39,56 @@
           lineHeight: "20px",
           padding: "7px 14px 6px",
         },
-        onClick: function () {
-          $('div.ibbr-table-row').each(function () {
-            const text = $(this).find('> span.ibbr-table-cell > span')
-              .map(function () {
-                return $(this).text().trim().replace(/[^A-Za-z0-9]/g, '_').replace('/\n/g', '');
-              })
-              .get()
-              .join('_') + ".pdf";
+        onClick: async function() {
+          if (loading) {
+            abort = true;
+            return;
+          }
+          
+          loading = true;
+          
+          try {
+            let downloaded = 0;
+            const rows = $('div.ibbr-table-row');
             
-            const url = "https://banking.ing.de/app" + $(this).find('a:contains(Download)').first().attr('href').substring(1);
-            GM_download(url, text);
-          });
+            const setProgress = () => {
+              downloaded += 1;
+              this.innerHTML = `${downloaded} / ${rows.length} verarbeitet (erneut klicken um abzubrechen)`;
+            };
+
+            const downloads = 
+              rows
+                .map(function () {
+                  const name = $(this).find('> span.ibbr-table-cell > span')
+                    .map(function () {
+                      return $(this).text().trim().replace(/[^A-Za-z0-9]/g, '_').replace('/\n/g', '');
+                    })
+                    .get()
+                    .join('_') + ".pdf";
+
+                  const url = "https://banking.ing.de/app" + $(this).find('a:contains(Download)').first().attr('href').substring(1);
+                  return { url, name };
+                })
+                .get();
+            
+            for (const d of downloads) {
+              if (abort) {
+                break;  
+              }
+              
+              await download(d.url, d.name);
+              setProgress();
+            }
+          } catch (err) {
+            console.log(err.stack);
+            alert("Es ist ein Fehler aufgetreten.");
+          }
+          
+          abort = false;
+          loading = false;
+          this.innerHTML = NAME;
         }
-      }, "Alle herunterladen"));
+      }, NAME));
     })
   })();
   
